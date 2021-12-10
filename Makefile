2fbe46d0d93046787f0a2b2d01f6d43355cadc20
Jon Barber <jon.barber@acm.org>
2021-12-10 12:03:41 +0000

    working on provided.al2 runtime

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 .PHONY: build clean deploy
 
 build:
-	npx sls build
+	CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o bin/bootstrap hello/main.go
+	zip -j bin/hello.zip bin/bootstrap
 
 clean:
 	rm -rf ./bin
 
 deploy:
 	aws-vault exec ninedemons-admin_role -- npx sls deploy --verbose
+
+all: clean build deploy
--- hello/main.go
+++ hello/main.go
@@ -4,10 +4,49 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/sethvargo/go-envconfig"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
+
+type Environment struct {
+	Application   string `env:"APPLICATION,required"`
+	Environment   string `env:"ENVIRONMENT,required"`
+	Configuration string `env:"CONFIGURATION,required"`
+}
+
+func (c Environment) ConfigURI() string {
+	return fmt.Sprintf("http://localhost:2772/applications/%s/environments/%s/configurations/%s", c.Application, c.Environment, c.Configuration)
+}
+
+func getConfiguration(ctx context.Context, uri string) (string, error) {
+	log.Printf("Getting config from %s", uri)
+	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return "", err
+	}
+	req = req.WithContext(ctx)
+
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	//Convert the body to type string
+	return string(body), nil
+}
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
 // AWS Lambda Proxy Request functionality (default behavior)
@@ -15,31 +54,50 @@ import (
 // https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
-// Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
-	var buf bytes.Buffer
+func withEnv(c Environment) func(ctx context.Context) (Response, error) {
+	return func(ctx context.Context) (Response, error) {
 
-	body, err := json.Marshal(map[string]interface{}{
-		"message": "Go Serverless v1.0! Your function executed successfully!",
-	})
-	if err != nil {
-		return Response{StatusCode: 404}, err
+		conf, err := getConfiguration(ctx, c.ConfigURI())
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+
+		log.Printf("Configuration : %s\n", conf)
+
+		var buf bytes.Buffer
+
+		body, err := json.Marshal(map[string]interface{}{
+			"config": conf,
+		})
+		if err != nil {
+			return Response{StatusCode: 404}, err
+		}
+		json.HTMLEscape(&buf, body)
+
+		r := Response{
+			StatusCode:      200,
+			IsBase64Encoded: false,
+			Body:            buf.String(),
+			Headers: map[string]string{
+				"Content-Type":           "application/json",
+				"X-MyCompany-Func-Reply": "hello-handler",
+			},
+		}
+
+		return r, nil
 	}
-	json.HTMLEscape(&buf, body)
-
-	resp := Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "hello-handler",
-		},
-	}
-
-	return resp, nil
 }
 
 func main() {
-	lambda.Start(Handler)
+
+	ctx := context.Background()
+
+	var c Environment
+	if err := envconfig.Process(ctx, &c); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Configuration : %+v", c)
+
+	lambda.Start(withEnv(c))
 }
